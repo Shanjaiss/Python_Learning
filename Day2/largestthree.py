13ee63f2fb06fb2b92fb9b1101b1bf302bafdd5d
Shanjaiss <shanjaisenthil10@gmail.com>
2025-12-23 22:44:23 +0530

    Day 2 if else

---
+++ Day2/largestthree.py
@@ -0,0 +1,11 @@
+x = int(input("Enter x Number : "))
+y = int(input("Enter y Number : "))
+z = int(input("Enter z Number : "))
+
+
+if x > y :
+  print(x,"is largest number")
+elif y > z:
+  print(y,"is the largest number")
+else :
+  print(z, "is largest number")
